c07114d3bcf96f31f9eb97f660dbf5cc0ea448a4
Piyush Acharya <acharyapiyush1@outlook.com>
2023-06-28 16:23:25 -0700

    Finish player

--- CinematicVue/ViewController.swift
+++ CinematicVue/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -13,7 +15,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        playVideo()
+    }
 
-
+    private func playVideo() {
+        guard let path = Bundle.main.path(forResource: "IMG_0017", ofType: "mov") else {
+            debugPrint("IMG_0017.mov not found")
+            return
+        }
+        
+        let player = AVPlayer(url: URL(fileURLWithPath: path))
+        let playerController = AVPlayerViewController()
+        playerController.player = player
+        present(playerController, animated: true) {
+            player.play()
+        }
+    }
 }
 
