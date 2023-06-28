387a2c9851f5668a07ccdbd4d72678d446e56a14
Piyush Acharya <acharyapiyush1@outlook.com>
2023-06-28 13:56:54 -0700

    Initial Commit

---
+++ CinematicVue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CinematicVue
+//
+//  Created by Piyush Acharya on 6/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
